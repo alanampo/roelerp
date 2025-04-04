69a70806f7159db60634f87e721610dfc40a4c83
Alan Ampo <alanampo@gmail.com>
2025-04-04 21:33:50 +0200

    aaaa

--- class_lib/sesionSecurity.php
+++ class_lib/sesionSecurity.php
@@ -1,5 +1,24 @@
 <?php
 ///****ARCHIVO DE FUNCIONES*****///
+$filePath = $_SERVER['DOCUMENT_ROOT'] . '/.env';
+if (!file_exists($filePath)) {
+  throw new Exception("Archivo .env no encontrado.");
+}
+
+$lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+foreach ($lines as $line) {
+  if (strpos($line, '=') !== false) {
+    list($name, $value) = explode('=', $line, 2);
+    $name = trim($name);
+    $value = str_replace('"', '', trim($value));
+    
+    if (!array_key_exists($name, $_ENV)) {
+      putenv("$name=$value");
+      $_ENV[$name] = $value;
+      $_SERVER[$name] = $value;
+    }
+  }
+}
 
 session_name("roel-erp");
 session_start();
