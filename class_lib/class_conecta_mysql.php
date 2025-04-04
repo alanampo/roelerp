5b349745790108d8f41e8bd210beaa7f1159f167
Alan Ampo <alanampo@gmail.com>
2025-04-04 21:29:12 +0200

    aaaa

--- class_lib/class_conecta_mysql.php
+++ class_lib/class_conecta_mysql.php
@@ -2,17 +2,18 @@
 error_reporting(0);
 
 if (strpos($_SERVER['HTTP_HOST'], 'roelplant') !== false) {
-  $host = "127.0.0.1"; /* Host name */
-  $user = "roeluser1_usercli"; /* User */
-  $password = "SergioVM2022!!"; /* Password */
-  $dbname = "roeluser1_bdsys"; /* Database name */
+  $host = getenv("DB_HOST");
+  $user = getenv("DB_USER");
+  $password = getenv("DB_PASSWORD");
+  $dbname = getenv("DB_NAME");
+  $dbpresta = getenv("DB_NAME_PRESTASHOP");
 }
 else{
-  $host = "127.0.0.1"; /* Host name */
-  $user = "root"; /* User */
-  $password = ""; /* Password */
-  $dbname = "roel"; /* Database name */
+  $host = getenv("DB_HOST_LOCAL");
+  $user = getenv("DB_USER_LOCAL");
+  $password = getenv("DB_PASSWORD_LOCAL");
+  $dbname = getenv("DB_NAME_LOCAL");
+  $dbpresta = getenv("DB_NAME_PRESTASHOP_LOCAL");
 }
-
 
 ?>
--- class_lib/scripts.php
+++ class_lib/scripts.php
@@ -1,3 +1,26 @@
+<?php
+$filePath = $_SERVER['DOCUMENT_ROOT'] . '/.env';
+if (!file_exists($filePath)) {
+  throw new Exception("Archivo .env no encontrado.");
+}
+
+$lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+foreach ($lines as $line) {
+  if (strpos($line, '=') !== false) {
+    list($name, $value) = explode('=', $line, 2);
+    $name = trim($name);
+    $value = str_replace('"', '', trim($value));
+    
+    if (!array_key_exists($name, $_ENV)) {
+      putenv("$name=$value");
+      $_ENV[$name] = $value;
+      $_SERVER[$name] = $value;
+    }
+  }
+}
+
+?>
+
 <!-- jQuery 3.1.1 -->
 <script src="./js/globals.js?v=<?php echo $version ?>" type="text/javascript"></script>
 <script src="./js/jquery.min.js" type="text/javascript"></script> 
