13af4e39040a52640707f5403bb9b50cdab56d09
Alan Ampo <alanampo@gmail.com>
2025-04-12 19:18:51 +0200

    aaaa

--- class_lib/sesionSecurity.php
+++ class_lib/sesionSecurity.php
@@ -23,7 +23,7 @@ foreach ($lines as $line) {
 session_name("roel-erp");
 session_start();
 date_default_timezone_set("America/Santiago");
-$version = "57";
+$version = "59";
 header('Content-type: text/html; charset=utf-8');
 if (!isset($_SESSION["roel-erp-token"]) || !isset($_COOKIE["roel-erp-token"])) {
   header("Location: index.php");
