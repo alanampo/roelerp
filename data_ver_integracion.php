a7cfe1ef993515b384694796325c0d92d3b62170
Alan Ampo <alanampo@gmail.com>
2025-04-29 19:48:49 +0200

    aaaa

--- data_ver_integracion.php
+++ data_ver_integracion.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 require "./class_lib/sesionSecurity.php";
 require 'class_lib/class_conecta_mysql.php';
 require 'class_lib/funciones.php';
